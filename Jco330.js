38b1fa15d85dcf79e3fdf3dc417b77557dded9a3
Jco17 <99445381+Jco17@users.noreply.github.com>
2022-02-11 01:48:00 -0500

    Update Jco330.js

--- Jco330.js
+++ Jco330.js
@@ -77,7 +77,11 @@ switch (command) {
 case 'bot':
 client.sendMessage(from, 'Hola,felicidades, has logrado enviar un mensaje mediante un servidor externo😚', text, {quoted : jco})
 break
-                
+
+case 'menu':const imagen = fs.readFileSync('media/logo.jpg')client.sendMessage(from, imagen, MessageType.image, {quoted: { key: {fromMe: false,participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})},message: {"documentMessage": { "title": "By 𝕵.𝕮.𝕺", 'jpegThumbnail': fs.readFileSync('media/logo.jpg)}}}})break 
+
+
+               
 }
 
 } catch (e) {
