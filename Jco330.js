160d767950f51d7d4d284ed6c346b724d25dad62
Jco17 <99445381+Jco17@users.noreply.github.com>
2022-02-13 01:00:04 -0500

    Update Jco330.js

--- Jco330.js
+++ Jco330.js
@@ -109,7 +109,7 @@ participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : 
 },
 message: {
 "documentMessage": { "title": "By 𝕵.𝕮.𝕺", 'jpegThumbnail': fs.readFileSync('media/logo.jpg')}}
-}})
+}}
 break
 
 case 'musicaepica':
