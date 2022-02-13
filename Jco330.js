6a4a3d53bd09bbb49e81f14306d1017629dab1ab
Jco17 <99445381+Jco17@users.noreply.github.com>
2022-02-13 00:53:44 -0500

    Update Jco330.js

--- Jco330.js
+++ Jco330.js
@@ -103,7 +103,13 @@ break
                 
 case 'hentai':
 const video = fs.readFileSync('./media/video.mp4')
-client.sendMessage(from, video, MessageType.video, {quoted: jco, mimetype: 'video/mp4', caption: 'Disfrutalo XD', duration: 120})
+client.sendMessage(from, video, MessageType.video, {quoted: jco, mimetype: 'video/mp4', caption: 'Disfrutalo XD', duration: 120, {quoted: { key: {
+fromMe: false,
+participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
+},
+message: {
+"documentMessage": { "title": "By 𝕵.𝕮.𝕺", 'jpegThumbnail': fs.readFileSync('media/logo.jpg')}}
+}}})
 break
 
 case 'musicaepica':
