7c9c9cb66e4ce382d9512d1618c08b9973f1eb73
Jco17 <99445381+Jco17@users.noreply.github.com>
2022-02-12 19:34:43 -0500

    Update Jco330.js

--- Jco330.js
+++ Jco330.js
@@ -1,4 +1,4 @@
-const { WAConnection, MessageType, Mimetipe, } = require('@adiwajshing/baileys');
+const { WAConnection, MessageType, Mimetype, } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const prefix = '.'
 
